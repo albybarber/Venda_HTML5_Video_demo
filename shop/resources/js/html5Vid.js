79648dc6057030259a15b9b4b2b8f039196eb7f5
alby barber <abarber@venda.com>
2011-05-20 15:47:17 +0100

    Updating script and page to use JSON style object

--- shop/resources/js/html5Vid.js
+++ shop/resources/js/html5Vid.js
@@ -1,119 +1,55 @@
 jQuery(document).ready(function(){
     
-	var v = document.getElementsByTagName('video')[0];
+	var v = document.getElementsByTagName('video')[0]; 
+	//var v = jQuery("video").get(0); // Getting the first video element on the page and assigning it to var 'v'
 
-	var prod1 = false;
-	var prod2 = false;
-	var prod3 = false;
-	
-	jQuery('video').bind('play', function()
-    {
-        //console.log('play event from bind listener');
-		
-        //works in all Browsers
-    });
-	
-	jQuery(document).ready(function(){
-    
-	var v = document.getElementsByTagName('video')[0];
-
-	var prod1 = false;
-	var prod2 = false;
-	var prod3 = false;
-	
-	jQuery('video').bind('play', function()
-    {
-        //console.log('play event from bind listener');
-		
-        //works in all Browsers
-    });
-	
-	jQuery('.catwalk .add').click(function() {
-		v.pause();
-	});
+// An array of objects that have time and sku values to show later.
+	var products = [ 
+		{
+			time	:	433,
+			sku		:	"eveningdress"
+		},
+		{
+			time	:	438,
+			sku		:	"hulabag"
+		},
+		{
+			time	:	441,
+			sku		:	"bottega"
+		}		
+	];
 	
 	jQuery('.skip').click(function() {
-		v.currentTime = 427;
-		v.play();
+		v.currentTime = 427;	// calling the 'currentTime' method on the video element 	- Set current time to value
+		v.play();				// calling the 'play' method on the video element			- Play the video
 
 	});
 	
-	jQuery('video').bind("timeupdate" ,function(){
+	function loadComp(){
+		console.log('you have a yoyo call back!	');
+		jQuery('#catwalkProducts').show();
+		
+	}
+	
+	jQuery('.catwalk video').bind("timeupdate" ,function(){ // Binding the 'timeupdate' event (HTML5 API) to the video element - This fires every 250ms
 			
-		time = v.currentTime;
-		var timeInSecs = parseInt(time);
-		
-		//console.log(time);
-		if (timeInSecs == 433 && prod1 == false) {
-				//console.log('YO: 435');
-				jQuery('#catwalkProducts .suggestedproduct1').append().load("http://abarber2.prod.venda.com/invt/eveningdress/&temp=catwalk&layout=blank");
-				prod1 = true;
-				jQuery('#catwalkProducts').show();
-		}
-		
-		if (timeInSecs == 438 && prod2 == false) {
-				//console.log('YO: 438');
-				jQuery('#catwalkProducts .suggestedproduct2').append().load("http://abarber2.prod.venda.com/invt/hulabag/&temp=catwalk&layout=blank");
-				prod2 = true;
-		}
-		
-		if (timeInSecs == 439 && prod3 == false) {
-				//console.log('YO: 443');
-				jQuery('#catwalkProducts .suggestedproduct3').append().load("http://abarber2.prod.venda.com/invt/bottega/&temp=catwalk&layout=blank");
-				prod3 = true;
-		}
-		
-				
-		
-        //works in all Browsers
-		
+		var time = v.currentTime;			// Getting the current time of the video+9
+		var timeInSecs = parseInt(time);	// Return an in whole second
 
-
+		var toAdd = jQuery.grep(products, function(prod) { // using the .grep method to go through the array and returning a object that matches the current time
+			return prod.time == timeInSecs; 
+		});
+		
+		
+		// Simple if statments to check the time and 'load' via AJAX the products using a blank layout and a customised template.  
+		if(toAdd.length == 1) {
+			var prod = toAdd[0];
+			var url = 'http://showcase.uat.venda.com/invt/' + prod.sku + '/&temp=catwalk&layout=blank';
+			
+			jQuery('#catwalkProducts').prepend("<div class='inner'></div>"); // creating a home for our new product
+			jQuery('#catwalkProducts .inner:first-child').load(url,loadComp); // ajax loading the products using pre-made url in the home
+			products.shift(); // shifting the current product object out of the array (so that it is not called again in the same second)
+		}
 	});
-
 	
 });
-  
-
-	
-	jQuery('.skip').click(function() {
-		v.currentTime = 427;
-		v.play();
-	});
-	
-	jQuery('video').bind("timeupdate" ,function(){
-			
-		time = v.currentTime;
-		var timeInSecs = parseInt(time);
-		
-		//console.log(time);
-		if (timeInSecs == 433 && prod1 == false) {
-				//console.log('YO: 435');
-				jQuery('#catwalkProducts .suggestedproduct1').append().load("http://abarber2.prod.venda.com/invt/eveningdress/&temp=catwalk&layout=blank");
-				prod1 = true;
-				jQuery('#catwalkProducts').show();
-		}
-		
-		if (timeInSecs == 438 && prod2 == false) {
-				//console.log('YO: 438');
-				jQuery('#catwalkProducts .suggestedproduct2').append().load("http://abarber2.prod.venda.com/invt/hulabag/&temp=catwalk&layout=blank");
-				prod2 = true;
-		}
-		
-		if (timeInSecs == 439 && prod3 == false) {
-				//console.log('YO: 443');
-				jQuery('#catwalkProducts .suggestedproduct3').append().load("http://abarber2.prod.venda.com/invt/bottega/&temp=catwalk&layout=blank");
-				prod3 = true;
-		}
-		
-				
-		
-        //works in all Browsers
-		
-
-
-	});
-
-	
-});
-  
